25e1cf5aeed5c63c8a4c6a64072d1cccf5735da2
JB <jandersonbrito72@gmail.com>
2020-03-06 17:18:24 -0300

    Rotas

--- app.js
+++ app.js
@@ -3,12 +3,23 @@
     const handlebars = require('express-handlebars');
     const bodyParser = require('body-parser');
     const app = express();
+    const admin = require('./routes/admin')
     // const mongoose = require('mongoose');
     
 // Configurações
-    
-    
+    // Body Parser
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(bodyParser.json());
+    // Handlebars
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+        app.set('view engine', 'handlebars');
+    // Mongoose
+
+    // 
 // Rotas
-
+    app.use('/admin', admin);
 // Outros
-
+const Port = 8081
+app.listen(Port, () => {
+    console.log('Servidor Rodando!')
+});
